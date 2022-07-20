abd2b95adae8c6f16412752818f9ad20d3454b41
Alex Christian <alexchristianqr@gmail.com>
2022-07-20 02:31:21 -0500

    [updated] sh

--- release.sh
+++ release.sh
@@ -15,10 +15,6 @@ set -e
 git tag -a -m "new tag release v$tagVersion" "v$tagVersion"
 git push origin "v$tagVersion"
 
-npm run build
-
-cd dist
-
 git init
 git add -A
 git commit -m "new deployment for release v$tagVersion"
