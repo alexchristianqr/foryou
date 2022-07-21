6b30273f5690d5515e7bca4252262d5392bc7948
Alex Christian <alexchristianqr@gmail.com>
2022-07-21 02:28:07 -0500

    [updated] release

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# This is the automated release script
 
-# guard against stupid
 if [ -z "$1" ]; then
    echo "You must specify a new version level: [patch, minor, major]";
    exit 1;
@@ -11,12 +9,9 @@ tagVersion=$1
 
 set -e
 
-#git tag -d "v$tagVersion"
 git tag -a -m "new tag release v$tagVersion" "v$tagVersion"
 git push origin "v$tagVersion"
 
-#git commit -m "new deployment for release v$tagVersion"
-#git push origin -f gh-pages
-git push -f git@github.com:alexchristianqr/foryou.git master:gh-pages
+git push -f git@github.com:alexchristianqr/foryou.git main:gh-pages
 
 cd -
