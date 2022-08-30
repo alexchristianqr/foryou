d3d2e1ef6b78959353dc5795403c48b9e67f9340
Alex Christian <alexchristianqr@gmail.com>
2022-08-30 14:54:01 -0500

    [updated] refactor reproductor

--- src/components/foryou/layouts/Reproductor.vue
+++ src/components/foryou/layouts/Reproductor.vue
@@ -2,6 +2,9 @@
   <div class="position-relative mt-5">
     <div class="text-left py-3 px-3 position-fixed" style="bottom: 0; left: 0; right: 0">
       <div class="text-center">
+        <b-button v-if="displayAction.action == 'frases' && fileAudio.isPlaying" @click="randomSounds" variant="transparent" size="lg" class="my-auto button-foryou mr-1">
+          <b-icon icon="shuffle"></b-icon>
+        </b-button>
         <b-button v-if="fileAudio.isPlaying" @click="pauseAudio()" class="my-auto button-foryou" size="lg" variant="transparent">
           <b-row>
             <b-col cols="3" class="my-auto">
@@ -30,7 +33,7 @@
             </b-col>
           </b-row>
         </b-button>
-        <b-button v-if="!fileAudio.isPlaying" @click="playAudio(labelSound)" class="my-auto button-foryou" size="lg" variant="transparent">
+        <b-button v-if="!fileAudio.isPlaying" @click="playAudio()" class="my-auto button-foryou" size="lg" variant="transparent">
           <b-row>
             <b-col cols="3" class="my-auto">
               <b-icon icon="play"></b-icon>
@@ -55,33 +58,75 @@ export default {
   props: {
     fileAudio: Object,
     displayAction: Object,
-    labelMusic: String,
   },
   data: () => ({
     timerInterval: null,
+    dataSounds: ['1.mp3', '2.mp3', '3.mp3'],
+    currentLabelSound: null,
   }),
-  computed:{
-    labelSound(){
+  computed: {
+    labelSound() {
       return '1.mp3'
-    }
+    },
   },
   mounted() {
     const folder = this.displayAction.action
     let labelSound = null
     switch (folder) {
       case 'frases':
-        labelSound = '1.mp3'
-        break
       case 'chistes':
         labelSound = '1.mp3'
+        this.currentLabelSound = labelSound
+        this.defaultAudio({ folder, labelSound })
         break
       default:
         console.error('Not support case action')
         break
     }
-    this.defaultAudio({ folder, labelSound })
   },
   methods: {
+    getRandomIndex() {
+      // Obtener lista de musicas filtradas
+      const listSounds = this.dataSounds.filter((item) => item != this.currentLabelSound)
+
+      // Obtener random
+      let randomNumber = Math.floor(Math.random() * listSounds.length)
+
+      // Retornar una musica aleatoria filtrada
+      return listSounds[randomNumber]
+    },
+    shuffle() {
+      let array = this.dataSounds
+
+      let copy = [],
+        n = array.length,
+        i
+
+      // While there remain elements to shuffle…
+      while (n) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * array.length)
+
+        // If not already shuffled, move it to the new array.
+        if (i in array) {
+          copy.push(array[i])
+          delete array[i]
+          n--
+        }
+      }
+
+      // return i
+      return this.dataSounds[i - 1]
+    },
+    randomSounds() {
+      // Set
+      const labelSound = this.getRandomIndex()
+      this.currentLabelSound = labelSound
+      const folder = this.displayAction.action
+
+      // Reproducir sonido
+      this.defaultAudio({ folder, labelSound, startPlaying: true })
+    },
     defaultAudio({ folder, labelSound, startPlaying = false }) {
       const getSound = () => {
         return require(`@/assets/sounds/${folder}/${labelSound ? labelSound : '1.mp3'}`)
@@ -89,7 +134,8 @@ export default {
       const src = getSound()
       if (!src) return
 
-      if (!this.fileAudio.audio) {
+      if (!this.fileAudio.audio || startPlaying) {
+        if (startPlaying) this.fileAudio.audio.pause() // Pausar musica anterior
         this.fileAudio.audio = new Audio(src)
         if (!this.fileAudio.audio) return
       }
